4623d4b9eec0001908c27315c0a50bdf9e9f61b0
ashercs <105057569+ashercs@users.noreply.github.com>
2023-02-10 20:21:32 -0500

    Update aoty.js

--- aoty.js
+++ aoty.js
@@ -13102,7 +13102,8 @@ var aoty = (() => {
         method: "GET",
         uri: url,
         headers: {
-          "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+          "user-agent": "Test",
+          "User-Agent": "Testing"
         }
       });
       window.sendCosmosRequest({
